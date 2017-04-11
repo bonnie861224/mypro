35cba381363e546ffed9406121e395284154e5ef
user <user@es302n29>
2017-04-11 14:15:11 +0800

    發票兌獎

--- src/oo/abstraction/Invoice.java
+++ src/oo/abstraction/Invoice.java
@@ -6,9 +6,9 @@ import java.util.Scanner;
 public class Invoice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in);
+		// TODO Auto-generated method stub		
 		System.out.println("請輸入你的發票號碼:");
+		Scanner scanner = new Scanner(System.in);
 		String s = scanner.nextLine();
 		// ---------0123456789
 		System.out.println(s.length());
@@ -16,17 +16,17 @@ public class Invoice {
 		// String sub = n.substring(7);
 		String sub = s.substring(s.length() - 3);
 		System.out.println(sub);
-		ArrayList<String> a = new ArrayList();
-		a.add("331");
-		a.add("821");
-		a.add("886");
-		a.add("554");
-		if(a.equals(sub)){
-			
-		}else{
-			
+		ArrayList<Integer> a = new ArrayList();
+		a.add(331);
+		a.add(821);
+		a.add(886);
+		a.add(554);
+		int i = Integer.parseInt(sub);
+		if (i == a.get(0) || i == a.get(1) || i == a.get(2) || i == a.get(3)) {
+			System.out.println("中獎");
+		} else {
+			System.out.println("沒中");
+
 		}
-		
 	}
 }
-
