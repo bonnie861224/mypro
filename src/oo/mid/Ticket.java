b0fc7cb90fb6d97a7e2af013ce959fe323fe756b
user <user@es302n29>
2017-04-25 14:31:56 +0800

    midterm exam practice

--- src/oo/mid/Ticket.java
+++ src/oo/mid/Ticket.java
@@ -4,28 +4,33 @@ import java.util.ArrayList;
 
 public class Ticket {
 
+	private static final ArrayList<Integer> a = null;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int id;
 		String name;
 		float price = 1000;
-		float price1=1500;
-	
-	ArrayList<Integer> a = new ArrayList();
-	a.add(012);
-	a.add(456);
-	a.add(789);
-	a.set(0, 123);
-	ArrayList<String> b = new ArrayList();
-	b.add("Superman");
-	b.add("Spiderman");
-	b.add("Batman");
-	b.set(1, "Thor");
-	ArrayList<Float> c = new ArrayList();
-	c.add(price);
-	c.add(price);
-	c.add(price);
-	c.set(2, price1);
+		float price1 = 1500;
+
+		ArrayList<Integer> a = new ArrayList();
+		a.add(012);
+		a.add(456);
+		a.add(789);
+		a.set(0, 123);
+		ArrayList<String> b = new ArrayList();
+		b.add("Superman");
+		b.add("Spiderman");
+		b.add("Batman");
+		b.set(1, "Thor");
+		ArrayList<Float> c = new ArrayList();
+		c.add(price);
+		c.add(price);
+		c.add(price);
+		c.set(2, price1);
+
+		System.out.print(a.get(0));
+		System.out.print(b.get(1));
+		System.out.print(c.get(2));
 	}
-	
 }
