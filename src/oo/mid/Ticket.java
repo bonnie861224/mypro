66a8e80f7694e76ae22920d8bd63224bd956d35a
bonnie861224 <b22379222@gmail.com>
2017-04-30 14:48:35 +0800

    midterm exam practice

--- src/oo/mid/Ticket.java
+++ src/oo/mid/Ticket.java
@@ -1,36 +1,32 @@
 package oo.mid;
 
-import java.util.ArrayList;
-
 public class Ticket {
+	// TODO Auto-generated method stub
+	int id;
+	String name;
+	float price;
 
-	private static final ArrayList<Integer> a = null;
+	public void setId(int id) {
+		this.id = id;
+	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int id;
-		String name;
-		float price = 1000;
-		float price1 = 1500;
+	public int getId() {
+		return id;
+	}
 
-		ArrayList<Integer> a = new ArrayList();
-		a.add(012);
-		a.add(456);
-		a.add(789);
-		a.set(0, 123);
-		ArrayList<String> b = new ArrayList();
-		b.add("Superman");
-		b.add("Spiderman");
-		b.add("Batman");
-		b.set(1, "Thor");
-		ArrayList<Float> c = new ArrayList();
-		c.add(price);
-		c.add(price);
-		c.add(price);
-		c.set(2, price1);
+	public void setName(String name) {
+		this.name = name;
+	}
 
-		System.out.print(a.get(0));
-		System.out.print(b.get(1));
-		System.out.print(c.get(2));
+	public String getName() {
+		return name;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public Float getPrice() {
+		return price;
 	}
 }
