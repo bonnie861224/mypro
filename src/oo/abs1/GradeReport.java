6fd6c8413cfd70299544985704e95d7b13e0cfe0
user <user@es302n36>
2017-06-06 16:06:09 +0800

    abstract practice

---
+++ src/oo/abs1/GradeReport.java
@@ -0,0 +1,17 @@
+package oo.abs1;
+
+public class GradeReport extends Report {
+
+	@Override
+	public void test() {
+		// TODO Auto-generated method stub
+		super.test();
+	}
+
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+		super.print();
+	}
+
+}
---
+++ src/oo/abs2/Tester.java
@@ -0,0 +1,11 @@
+package oo.abs2;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Report report = new Report(); report是抽象的不能產生
+	    GradeReport greport = new GradeReport();
+	}
+
+}
